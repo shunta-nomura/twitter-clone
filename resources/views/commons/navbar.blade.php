c18d7766bdb814c50d42a4fbcdbc1a7e74f38c2f
shunta.nomura <ns_is_a_pen@icloud.com>
2019-12-17 07:35:18 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -15,3 +15,8 @@
         </div>
     </nav>
 </header>
+
+    <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li><a href="#">Login</a></li>
+                </ul>
